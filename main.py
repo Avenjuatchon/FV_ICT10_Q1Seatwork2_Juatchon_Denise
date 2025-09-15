02fc262e881a7e24018c80917a256c974ab72bd8
Avenjuatchon <juatchon.dg@obmontessori.edu.ph>
2025-09-15 16:54:41 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ display(f"Owner: {owner}", target="ownerName")
 display(f"Est. {year_founded}", target="ownerName")
 
 
+# I don't understand anything ma'am i'm so sorry po, I tried many
+# things na po and it's not working po. I'm really sorry po ma'am.
