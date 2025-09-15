b7133c6b70ab9bc6c7c32256f2f8e6640da238ab
Avenjuatchon <juatchon.dg@obmontessori.edu.ph>
2025-09-15 16:54:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from pyscript import display
+
+store_name= "Ivie's Tea Shop" 
+owner = 'Denise Juatchon' # String
+year_founded = 2025 # int
+
+display(store_name, target="storeName")
+display(f"Owner: {owner}", target="ownerName")
+display(f"Est. {year_founded}", target="ownerName")
+
+
